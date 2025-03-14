0b56a6ce5502eefab0612b7a06b75da1be5c3864
aaalzand <48525345+aaalzand@users.noreply.github.com>
2025-03-14 10:03:17 -0400

    First commit

---
+++ docusaurus/docusaurus.config.js
@@ -0,0 +1,205 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import { themes as prismThemes } from 'prism-react-renderer';
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Technical notes for Microsoft workloads on AWS',
+  tagline: '🖥️ Improve Microsoft workloads on AWS 🚀',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://aws-msft.github.io',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/technical-notes-for-microsoft-workloads-on-aws/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'AWS', // Usually your GitHub org/user name.
+  projectName: 'Technical notes for Microsoft workloads on AWS', // Usually your repo name.
+
+  onBrokenLinks: 'warn',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          routeBasePath: '/',
+          sidebarPath: './sidebars.js',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/aws-samples/technical-notes-for-microsoft-workloads-on-aws/blob/main/docusaurus/',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      }),
+    ],
+  ],
+
+  plugins: [
+
+    [
+      require.resolve("@easyops-cn/docusaurus-search-local"),
+      ({
+        //docsDir: "docs",
+        hashed: true,
+        indexPages: true,
+        language: ["en"],
+        indexBlog: false,
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Technical notes for Microsoft workloads on AWS',
+        logo: {
+          alt: 'AWS Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'home',
+            position: 'left',
+            label: 'Home',
+          },
+          {
+            type: 'doc',
+            docId: 'guides/index',
+            position: 'left',
+            label: 'Guides',
+          },
+          {
+            type: 'doc',
+            docId: 'tools/index',
+            position: 'left',
+            label: 'Tools',
+          },
+          {
+            type: 'doc',
+            docId: 'recipes/index',
+            position: 'left',
+            label: 'Recipes',
+          },
+          {
+            type: 'doc',
+            docId: 'faq/index',
+            position: 'left',
+            label: 'FAQ',
+          },
+          {
+            type: 'doc',
+            docId: 'patterns/index',
+            position: 'left',
+            label: 'Patterns',
+          },
+          {
+            type: 'doc',
+            docId: 'resources/index',
+            position: 'left',
+            label: 'Resources',
+          },
+          {
+            type: 'doc',
+            docId: 'contributors',
+            position: 'left',
+            label: 'Contributors',
+          },
+          {
+            type: 'localeDropdown',
+            position: 'right',
+          },
+          {
+            href: 'https://github.com/aws-samples/technical-notes-for-microsoft-workloads-on-aws',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      docs: {
+        sidebar: {
+          hideable: true,
+          autoCollapseCategories: true,
+        }
+      },
+      colorMode: {
+          defaultMode: 'light',
+          disableSwitch: false,
+          respectPrefersColorScheme: true,
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Tutorial',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Stack Overflow',
+                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+              },
+              {
+                label: 'Discord',
+                href: 'https://discordapp.com/invite/docusaurus',
+              },
+              {
+                label: 'X',
+                href: 'https://x.com/docusaurus',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/facebook/docusaurus',
+              },
+            ],
+          },
+        ],
+        copyright: `Built with ❤️ at AWS. <br/> © ${new Date().getFullYear()}.  Amazon.com, Inc. or its affiliates. All Rights Reserved.`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
+};
+
+export default config;
